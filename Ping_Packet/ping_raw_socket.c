558fb2e8b640aa5279ab3455ac8b85bb18594293
minhlq2311 <156107897+minhlq2311@users.noreply.github.com>
2024-09-11 13:37:43 +0700

    Update ping_raw_socket.c

--- Ping_Packet/ping_raw_socket.c
+++ Ping_Packet/ping_raw_socket.c
@@ -1,92 +1,210 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include <getopt.h>
+#include <signal.h>
+#include <netdb.h>
+#include <ifaddrs.h>
 
-typedef unsigned char u_char;
-typedef unsigned short u_short;
+#define ICMP_HEADER_SIZE 8
+#include "../packet_header.h"
 
-#include "/home/minhlq2311/Documents/Core Net 2.0/packet_header.h"
-// Function to calculate checksum
-unsigned short checksum(void *b, int len) {
-    unsigned short *buf = b;
-    unsigned int sum = 0;
+int sockFd;
+int isHost = -1;
+
+// Calculate checksum for ICMP header
+unsigned short checksum(unsigned short *buf, int len) {
+    unsigned long sum = 0;
     unsigned short result;
-
-    for (sum = 0; len > 1; len -= 2)
+    while (len > 1) {
         sum += *buf++;
-    if (len == 1)
-        sum += *(u_char*)buf;
+        len -= 2;
+    }
+    if (len == 1) {
+        sum += *(unsigned char *)buf;
+    }
     sum = (sum >> 16) + (sum & 0xFFFF);
     sum += (sum >> 16);
     result = ~sum;
     return result;
 }
 
-// Create IP header
-void createIpHeader(struct ipHeader *ip, char *srcIp, char *destIp, int dataLen){
-    ip->iph_ihl = 5;
-    ip->iph_ver = 4;
-    ip->iph_tos = 0;
-    ip->iph_len = htons(sizeof(struct ipHeader) + sizeof(struct icmpHeader) + dataLen);
-    ip->iph_ident = htons(54321);
-    ip->iph_offset = 0;
-    ip->iph_ttl = 255;
-    ip->iph_protocol = IPPROTO_ICMP;
-    ip->iph_chksum = 0;
-    ip->iph_sourceip.s_addr = inet_addr(srcIp);
-    ip->iph_destip.s_addr = inet_addr(destIp);
-    ip->iph_chksum = checksum((u_short *)ip, sizeof(struct ipHeader));  
+// Check if a string is a valid IP address
+int is_ip_address(const char *str) {
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, str, &(sa.sin_addr));
+    if(result != 0) {
+        isHost = 0;
+        return 1;
+    }
+    return 0; // Return 0 if not a valid IP address
 }
 
 // Create ICMP header
-void createIcmpHeader(struct icmpHeader *icmp, int dataLen){
-    icmp->icmp_type = ICMP_ECHO;
-    icmp->icmp_code = 0;
-    icmp->icmp_chksum = 0;
-    icmp->id = htons(12345);
-    icmp->seq = htons(0);
-    icmp->icmp_chksum = checksum((u_short *)icmp, sizeof(struct icmpHeader) + dataLen);
+void createIcmpHeader(struct icmpHeader *icmp_hdr, int seq, int dataLen) {
+    icmp_hdr->icmp_type = ICMP_ECHO;
+    icmp_hdr->icmp_code = 0;
+    icmp_hdr->icmp_chksum = 0;
+    icmp_hdr->id = htons(12345);
+    icmp_hdr->seq = htons(seq);
+    icmp_hdr->icmp_chksum = checksum((unsigned short *)icmp_hdr, dataLen + ICMP_HEADER_SIZE);
 }
 
-int main(){
-    char buffer[1500] = {0};
-    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
-    if(sockfd < 0){
-        perror("Socket creation failed");
-        exit(1);
+// Print usage instructions
+void print_usage() {
+    printf("Usage: ping [-t] [-a] [-n count] destination\n");
+    printf("Options:\n");
+    printf("  -t\t\tPing the specified host until stopped.\n");
+    printf("  -a\t\tResolve addresses to hostnames.\n");
+    printf("  -n count\tNumber of echo requests to send.\n");
+}
+
+// Parse command line arguments
+void parse_args(int argc, char *argv[], int *continuous, int *resolveIpToHost, int *count, char **destIp) {
+    int opt;
+    while ((opt = getopt(argc, argv, "tan:")) != -1) {
+        switch (opt) {
+            case 't':
+                *continuous = 1;
+                break;
+            case 'a':
+                *resolveIpToHost = 1;
+                break;
+            case 'n':
+                *count = atoi(optarg);
+                break;
+            default:
+                print_usage();
+                exit(EXIT_FAILURE);
+        }
     }
 
-    char *srcIp= "10.0.2.15";
-    char *destIp = "8.8.8.8";
-    char *data = "Hello World";
-    int dataLen = strlen(data);
+    if (optind >= argc) {
+        print_usage();
+        exit(EXIT_FAILURE);
+    }
 
-    struct ipHeader *ip = (struct ipHeader *)buffer;
-    struct icmpHeader *icmp = (struct icmpHeader *)(buffer + sizeof(struct ipHeader));
+    *destIp = argv[optind];
+}
 
-    createIpHeader(ip, srcIp, destIp, dataLen);
-    createIcmpHeader(icmp, dataLen);
+// Resolve hostname to IP address
+char *resolve_hostname(const char *hostname) {
+    struct hostent *he = gethostbyname(hostname);
+    if (he == NULL) {
+        fprintf(stderr, "Could not resolve hostname: %s\n", hostname);
+        exit(EXIT_FAILURE);
+    }
+    return inet_ntoa(*(struct in_addr *)he->h_addr_list[0]);
+}
 
-    // Format dest address
-    struct sockaddr_in dest;
-    dest.sin_family = AF_INET;
-    dest.sin_addr.s_addr = ip->iph_destip.s_addr;
+// Send ICMP packet
+void send_icmp(int sockFd, struct icmpHeader *icmp, const char *data, int dataLen, int sequence, const char *srcIp, const char *destIp) {
+    createIcmpHeader(icmp, sequence, dataLen);
 
-    // Send packet
-    if(sendto(sockfd, buffer, ntohs(ip->iph_len), 0, (struct sockaddr *)&dest, sizeof(dest)) < 0){
+    struct sockaddr_in destAddr;
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = 0; // ICMP doesn't use port numbers
+    destAddr.sin_addr.s_addr = inet_addr(destIp);
+
+    if (sendto(sockFd, icmp, dataLen + ICMP_HEADER_SIZE, 0, (struct sockaddr *)&destAddr, sizeof(destAddr)) <= 0) {
         perror("Sendto failed");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
-    else{
-        printf("Packet sent successfully\n");
+}
+
+// Receive ICMP response
+void receive_icmp_response(int sockFd) {
+    struct sockaddr_in addr;
+    socklen_t addr_len = sizeof(addr);
+    char buf[1024];
+    
+    int bytesReceived = recvfrom(sockFd, buf, sizeof(buf), 0, (struct sockaddr *)&addr, &addr_len);
+    if (bytesReceived > 0) {
+        struct ipHeader *ipHeader = (struct ipHeader *)buf;
+        int ipHeaderLen = ipHeader->iph_ihl * 4;
+
+        struct icmpHeader *icmp= (struct icmpHeader *)(buf + ipHeaderLen);
+        int icmpHeaderLen = sizeof(struct icmpHeader);
+        int icmpDataLen = bytesReceived - ipHeaderLen - icmpHeaderLen;
+
+        if (icmp->icmp_type == ICMP_ECHOREPLY) {
+            printf("Reply from %s: bytes=%d TTL=%d\n", inet_ntoa(addr.sin_addr), icmpDataLen, ipHeader->iph_ttl);
+        } else {
+            printf("Received ICMP packet of type %d\n", icmp->icmp_type);
+        }
+    } else {
+        printf("Request timed out.\n");
+    }
+}
+
+// Resolve IP address to hostname
+char *resolve_ip_to_hostname(const char *ip_addr) {
+    struct sockaddr_in sa;
+    sa.sin_family = AF_INET;
+    inet_pton(AF_INET, ip_addr, &sa.sin_addr);
+    
+    char host[1024];
+    char service[20];
+    
+    int res = getnameinfo((struct sockaddr*)&sa, sizeof(sa), host, sizeof(host), service, sizeof(service), 0);
+    
+    if (res != 0) {
+        return NULL;  // Cannot resolve
+    }
+    
+    return strdup(host);  // Copy the resolved hostname
+}
+
+// Main function
+int main(int argc, char *argv[]) {
+    int continuous = 0, resolveIpToHost = 0, count = 4;
+    char *destIp = NULL;
+    int sequence = 0;
+    int pings_sent = 0;
+    char *data = "Hello";
+    int dataLen = strlen(data);
+    
+    // Parse command-line arguments
+    parse_args(argc, argv, &continuous, &resolveIpToHost, &count, &destIp);
+    
+    char *finalDestIp;
+    
+    // Determine if destination is IP or hostname
+    if (is_ip_address(destIp)) {
+        finalDestIp = destIp;
+        if (resolveIpToHost) {
+            printf("Pinging %s [%s] with %d bytes of data\n", resolve_ip_to_hostname(destIp), destIp, dataLen);
+        } else {
+            printf("Pinging %s with %d bytes of data\n", destIp, dataLen);
+        }
+    } else {
+        isHost = 1;
+        finalDestIp = resolve_hostname(destIp);
+        printf("Pinging %s [%s] with %d bytes of data\n", destIp, finalDestIp, dataLen);
+    }
+    
+    sockFd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (sockFd < 0) {
+        perror("Socket error");
+        return 1;
     }
 
-    close(sockfd);
+    struct icmpHeader icmp;
+    while (continuous || pings_sent < count) {
+        send_icmp(sockFd, &icmp, data, dataLen, sequence, NULL, finalDestIp);
+        receive_icmp_response(sockFd);
+
+        pings_sent++;
+        sequence++;
+        sleep(1);
+    }
+
+    // Release resources
+    close(sockFd);
     return 0;
 }
